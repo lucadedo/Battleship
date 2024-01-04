c99e30fb911ad7b8ab341169b10d788ab2422070
lucadedo <lucadedo99@icloud.com>
2024-01-04 10:02:18 +0800

    Made small type changes

--- ship.js
+++ ship.js
@@ -5,29 +5,30 @@ const Ship = function(length,sunk,hits) {
 
    this.hit = function() {
     if (this.sunk === true) { // if ship already dead
-        console.log("ship already sunk");
+        //console.log("ship already sunk");
         throw new Error('ship already sunk');
     }else{         // else add one hit and check if the ship isSunk()
-    this.hits++;
-    console.log("Ship hit! Total hits: " + this.hits);
-    this.isSunk();
+        this.hits++;
+        //console.log("Ship hit! Total hits: " + this.hits);
+        this.isSunk();
+        return `"Ship hit! Total hits: "${this.hits}`;
     };
    };
 
    this.isSunk = function() {
-    if (this.hits >= this.length) {
-        console.log("Ship Sunk! Total hits: " + this.hits);
-        this.sunk = true;   
-        return 'ship sunk';// for testing
-    }else{
-        console.log("Ship still live!");       
-    }; 
+        if (this.hits >= this.length) { 
+            //console.log("Ship Sunk! Total hits: " + this.hits);
+            this.sunk = true;   
+            return `'ship sunk Total hits: ' ${this.hits}`;// for testing
+        }else{
+            return "Ship still live!";       
+        }; 
    };
 };
 
 const myShip  = new Ship(3, false ,0)
 const myShip2  = new Ship(2, false ,0)
-console.log(myShip.length);
+console.log(myShip);
 // myShip.hit()
 // myShip.hit()
 // myShip.hit()
