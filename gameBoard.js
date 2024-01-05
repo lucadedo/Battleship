db6a1ade1e48ba6ca1a5b88877d42a531a71d8f4
lucadedo <lucadedo99@icloud.com>
2024-01-05 10:30:21 +0800

    Ship validate place function
    checking if the position is placeable

--- gameBoard.js
+++ gameBoard.js
@@ -13,14 +13,16 @@ const GameBoard = function() {
     
     const board = new Array(10).fill(null).map(() => Array(10).fill(null));// 2D board
 
-    const carrier5 = new Ship(5, true ,0);
-    const battleship4 = new Ship(4, true ,0);
-    const cruiser3 = new Ship(3, true,0);
-    const submarine3 = new Ship(3, true,0);
+    const carrier5 = new Ship(5, false ,0);
+    const battleship4 = new Ship(4, false ,0);
+    const cruiser3 = new Ship(3, false,0);
+    const submarine3 = new Ship(3, false,0);
     const destroyer2 =new Ship(2, false,0);
     
     let allShips = [carrier5,battleship4,cruiser3,submarine3,destroyer2];
     let targetShip;
+    let shipDirectionHorizontal = true;
+    let shipDirectionVertical = false;
 
 
     this.chooseShip = function(num){
@@ -30,17 +32,60 @@ const GameBoard = function() {
     this.placeShip = function(x ,y) {
 
         if (x >= 0 && x < 10 && y >= 0 && y < 10 ) {
-           
-            for (let i = 0; i < targetShip.length; i++){
-                board[x][i + y] = targetShip;
-            }
-            console.log(board);
-            
 
+            if (shipDirectionHorizontal) {
+                if (this.validateThisPlace(x)) {
+                    for (let i = 0; i < targetShip.length; i++){
+                        board[i + x][y] = targetShip; //place horizontal
+                    };
+                }else{
+                    console.log("validate error,cant palce here 2"); 
+                };
+                
+            }else if (shipDirectionVertical) {
+                if (this.validateThisPlace(y)) {
+                    for (let i = 0; i < targetShip.length; i++){
+                        board[x][i + y] = targetShip; //place vertical
+                    };
+            }else{
+                console.log("validate error,cant palce here 2"); 
+            };
+            };
+            //console.log(board);
+            
         }else{
             throw new Error('cant place here ');
-        }
-    }
+        };
+    };
+
+    this.validateThisPlace = function(xy) { //check x or y ,depend on what it takes ,is the same
+        if ((targetShip.length > 4 && xy >= 6) ||
+            (targetShip.length > 3 && xy >= 7) ||
+            (targetShip.length > 2 && xy >= 8) ||
+            (targetShip.length == 2 && xy == 9)) {
+                console.log('validate error,cant palce here 1');
+                return false
+            }else{
+                return true
+            };
+
+
+    };
+
+
+
+
+    this.changeShipDirection = function(){
+        if (shipDirectionHorizontal === true) {
+            shipDirectionVertical = true;
+            shipDirectionHorizontal = false;
+        }else{
+            shipDirectionHorizontal = true;
+            shipDirectionVertical = false;
+        };
+    };
+
+
     this.receiveAttack = function(x,y) {
         if (board[x][y] !== null && board[x][y] == targetShip) {
             board[x][y].hit();
@@ -66,20 +111,19 @@ const GameBoard = function() {
        if (allTrue) {
         console.log("game over");
        }
-        
-    }
+    };
     
-
-}
+};
 
 const gameBoard = new GameBoard();
-gameBoard.chooseShip(4);
-gameBoard.placeShip(0,2);
+gameBoard.chooseShip(0);
+gameBoard.changeShipDirection();
+gameBoard.placeShip(2,6);
 
-gameBoard.receiveAttack(0,2);
-gameBoard.receiveAttack(0,3);
+// gameBoard.receiveAttack(0,2);
+// gameBoard.receiveAttack(0,3);
 
-//gameBoard.displayBoard()
+gameBoard.displayBoard()
 
 
 
