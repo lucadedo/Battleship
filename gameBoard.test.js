91bfdd0d4a97a1374049d876765b42317c8f1042
lucadedo <lucadedo99@icloud.com>
2024-01-05 14:16:16 +0800

    receive attack test

--- gameBoard.test.js
+++ gameBoard.test.js
@@ -1,5 +1,56 @@
 const GameBoard = require("./gameBoard");
+//npm test .\gameBoard.test.js to test only this
 
-test.skip('it is not snowing', () => {
-    expect(inchesOfSnow()).toBe(0);
-  });
+const testgameBoard = new GameBoard();
+
+test('CHOOSE SHIP', () => {
+  expect(testgameBoard.chooseShip(0).length).toBe(5);
+  expect(testgameBoard.chooseShip(0).sunk).toBeFalsy();
+  expect(testgameBoard.chooseShip(0)).toMatchObject({
+          length: 5,
+          sunk: false,
+          hits: 0,
+          hit: expect.any(Function),
+          isSunk: expect.any(Function)
+        }
+      );
+});
+
+test('PLACE SHIP', () => {
+  testgameBoard.placeShip(0,0)
+  testgameBoard.changeShipDirection();
+  testgameBoard.placeShip(3,4)
+  let testBoard = testgameBoard.displayBoard()
+  expect(testBoard[0][0]).not.toBeNull();
+  expect(testBoard[0][0]).toMatchObject({
+        length: 5,
+        sunk: false,
+        hits: 0,
+        hit: expect.any(Function),
+        isSunk: expect.any(Function)
+      }
+    );
+  expect(testgameBoard.placeShip(1,0)).toBeFalsy();
+  expect(testgameBoard.placeShip(3,5)).toBeFalsy();
+  expect(() => testgameBoard.placeShip(10,10)).toThrow('cant place here ');
+});
+
+test('RECIEVE ATTACK', () => {
+  testgameBoard.placeShip(0,0)
+  testgameBoard.receiveAttack(0,0)
+  let testBoard2 = testgameBoard.displayBoard()
+
+  expect(testBoard2[0][0]).not.toBeNull();
+  expect(testgameBoard.chooseShip(0).hits).toBe(1);
+  testgameBoard.receiveAttack(1,0);
+  expect(testgameBoard.chooseShip(0).hits).toBe(2);
+  testgameBoard.receiveAttack(2,0);
+  expect(testgameBoard.chooseShip(0).hits).toBe(3);// hits = 3
+  testgameBoard.receiveAttack(3,0);
+  testgameBoard.receiveAttack(4,0);
+  expect(testgameBoard.chooseShip(0).hits).toBe(5);// hits = 3
+  expect(testgameBoard.chooseShip(0).sunk).toBeTruthy();
+  testgameBoard.receiveAttack(6,5);
+  expect(testgameBoard.displayBoard()[6][5]).toEqual({missedX:6,missedY:5});// missed hits
+});
+
