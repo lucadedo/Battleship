1232a7de2f9b7721ab33222a1b7ec3718be60ec6
lucadedo <lucadedo99@icloud.com>
2024-01-17 11:51:01 +0800

    creates 2 board inside & render from func

--- GameLoop.js
+++ GameLoop.js
@@ -1,14 +1,16 @@
 import { GameBoard } from './gameBoard.js';
-import { Player } from './Player.js';
+//import { Player } from './Player.js';
 
 
 
-const GameLoop = function(player) { 
-    
-    console.log(player);
+const GameLoop = function(player,PC) { 
+   
     
     const playerGameBoard = new GameBoard();
+    const enemyGameBoard = new GameBoard();
+    console.log(playerGameBoard);
 
+    
     this.startTurn = function () {
         while(!enemyGameBoard.allShipSunk() || !playerGameBoard.allShipSunk()){
             if (player.turn) {
@@ -30,8 +32,7 @@ const GameLoop = function(player) {
         console.log('STOP');
     };
 
-
-    this.buildPlayerboardTest = function () {
+    this.buildPlayerboard = function () {
        //horizontal
        playerGameBoard.chooseShip(0);
        playerGameBoard.placeShip(0,0);
@@ -46,33 +47,86 @@ const GameLoop = function(player) {
        playerGameBoard.chooseShip(4);
        playerGameBoard.placeShip(5,7);
        console.log('PLAYER BOARD:');                         
-       playerGameBoard.displayBoard();
+       //playerGameBoard.displayBoard();
      
+       const playergameBoardDiv = document.getElementById("playergameBoard-div");
+       const newPlayerGameboard = playerGameBoard.displayBoard();
+       newPlayerGameboard.forEach((row,rowIndex) => {
+        //console.log(newPlayerGameboard);
+        const rowDiv = document.createElement('div');
+        rowDiv.className = "x";
+        rowDiv.id = "x" + rowIndex;
+
+        row.forEach((cell, cellIndex) => {
+        
+        const cellDiv = document.createElement('div');
+        cellDiv.className = "y";
+        cellDiv.id = "y" + cellIndex;
+        if (cell !== null) {
+            cellDiv.classList.add("ship");
+        };
+        rowDiv.appendChild(cellDiv);
+
+       });
+      
+        playergameBoardDiv.appendChild(rowDiv);
+       
+        
+        });
+        console.log('PLAYER BOARD:',newPlayerGameboard);
     };
 
 
     this.buildBoardPC = function () {
         //horizontal
-        playerGameBoard.chooseShip(0);
-        playerGameBoard.placeShip(1,1);
-        playerGameBoard.chooseShip(1);
-        playerGameBoard.placeShip(4,3);
-        playerGameBoard.chooseShip(2);
-        playerGameBoard.placeShip(6,5);
+        enemyGameBoard.chooseShip(0);
+        enemyGameBoard.placeShip(1,1);
+        enemyGameBoard.chooseShip(1);
+        enemyGameBoard.placeShip(4,3);
+        enemyGameBoard.chooseShip(2);
+        enemyGameBoard.placeShip(6,5);
         //vertical
-        playerGameBoard.chooseShip(3);
-        playerGameBoard.changeShipDirection()
-        playerGameBoard.placeShip(1,4);
-        playerGameBoard.chooseShip(4);
-        playerGameBoard.placeShip(3,7);
-        console.log('PC BOARD:');
-        playerGameBoard.displayBoard();
+        enemyGameBoard.chooseShip(3);
+        enemyGameBoard.changeShipDirection()
+        enemyGameBoard.placeShip(1,4);
+        enemyGameBoard.chooseShip(4);
+        enemyGameBoard.placeShip(3,7);
+        
+        //playerGameBoard.displayBoard();
 
+        const enemygameBoardDiv = document.getElementById("enemygameBoard-div");
+        const newEnemyGameboard = enemyGameBoard.displayBoard();
+        newEnemyGameboard.forEach((row,rowIndex) => {
+            //console.log(newEnemyGameboard);
+            const rowDiv = document.createElement('div');
+            rowDiv.className = "x";
+            rowDiv.id = "x" + rowIndex;
     
+            row.forEach((cell, cellIndex) => {
+            
+            const cellDiv = document.createElement('div');
+            cellDiv.className = "y";
+            cellDiv.id = "y" + cellIndex;
+            if (cell !== null) {
+                cellDiv.classList.add("ship");
+            };
+            rowDiv.appendChild(cellDiv);
+    
+           });
+          
+            enemygameBoardDiv.appendChild(rowDiv);
+         
+            
+        });
+        console.log('PC BOARD:',newEnemyGameboard);
     };
+
+
     this.renderPlayerBoard = function () {
         return playerGameBoard.displayBoard();
-    }
+    };
+
+    
 
 };
 
